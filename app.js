5cf07d63ddf2ce65d7888ff006b7a802f3ffddae
mishtickle <hamishok@gmail.com>
2024-08-02 15:36:06 +1200

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", indexRouter);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
 
 module.exports = app;
