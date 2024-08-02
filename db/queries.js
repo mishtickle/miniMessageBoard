124512e4a8a56a2595da4f152a936a5b37dbf5ac
mishtickle <hamishok@gmail.com>
2024-08-02 13:48:57 +1200

    Update queries.js

--- db/queries.js
+++ db/queries.js
@@ -4,23 +4,7 @@ async function getAllMessages() {
     await pool.query("CREATE TABLE IF NOT EXISTS messages (message, name, date)")
     const { rows } = await pool.query("SELECT * FROM messages");
     return rows;
-  }
-  
-//   async function insertUsername(username) {
-//     await pool.query("INSERT INTO usernames (username) VALUES ($1)", [username]);
-//   }
-  
-//   async function getUsername(user){
-//       const username = await pool.query("SELECT * FROM usernames WHERE username = ($1)", [user])
-//       if (username.rows[0] == undefined){
-//           return 'User does not exist';
-//       }
-//       return username.rows[0].username;
-//   }
-  
-//   async function deleteUsers(){
-//       await pool.query("DELETE FROM usernames")
-//   }
+}
   
   module.exports = {
     getAllMessages,
