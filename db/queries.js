e4e6d946c1d26c6cbf69d063e59e2e7b7df8f90a
mishtickle <hamishok@gmail.com>
2024-08-02 13:47:22 +1200

    Added if not exists to queries.js

--- db/queries.js
+++ db/queries.js
@@ -1,6 +1,7 @@
 const pool = require('./pool');
 
 async function getAllMessages() {
+    await pool.query("CREATE TABLE IF NOT EXISTS messages (message, name, date)")
     const { rows } = await pool.query("SELECT * FROM messages");
     return rows;
   }
